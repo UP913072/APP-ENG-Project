cfd54794f20acc75416caa4f2a17e8f721323835
Kenneth Villanueva <73391844+UP913072@users.noreply.github.com>
2022-04-24 22:17:42 +0100

    fix

--- code/app.js
+++
@@ -1,6 +0,0 @@
-import server from '../server/server';
-const router = () => {
-  const main = document.getElementById('main-container');
-  main.innerHTML = server.render();
-};
-window.addEventListener('load', router);
