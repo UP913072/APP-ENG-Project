d46a4f75fb782897e53c9f6b5b49c86c068674c1
Kenneth Villanueva <73391844+UP913072@users.noreply.github.com>
2022-04-25 18:58:28 +0100

    stage3

--- code/index2.mjs
+++ code/index2.mjs
@@ -12,9 +12,16 @@ function postItems(itemsjson) {
   const imgPlaceholder = document.getElementById('imgPlaceholder');
   for (let i = 0; i < itemsjson.length; i++) {
     const image = document.createElement('img');
+    image.classList.add('image');
     image.src = itemsjson[i].img;
     console.log('test');
     imgPlaceholder.append(image);
+    const name = document.createElement('div');
+    name.textContent = itemsjson[i].name;
+    imgPlaceholder.append(name);
+    const price = document.createElement('div');
+    price.textContent = itemsjson[i].p;
+    imgPlaceholder.append(price);
   }
   postItems();
 }
